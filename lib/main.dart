8372bd7c50b508addd8eab00cb051494f9785b82
jgarpue395 <jgarpue395@g.educaand.es>
2022-11-08 14:06:41 +0100

    Inicio

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/screens/details_screen.dart';
+import 'package:peliculas/screens/home_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: "home",
+      routes: {
+        "home": (context) => const HomeScreen(),
+        "details": (context) => const DetailsScreen() 
+      },
+    );
+  }
+}
